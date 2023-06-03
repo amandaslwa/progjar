7350d16d88b24270da7b9be51bb960b9da19733f
Amanda Salwa Salsabila <90702710+amandaslwa@users.noreply.github.com>
2023-06-03 17:21:22 +0700

    Update threadpool_client.py

--- Tugas 3/threadpool_client.py
+++ Tugas 3/threadpool_client.py
@@ -5,23 +5,23 @@ from concurrent.futures import ThreadPoolExecutor
 
 def kirim_request():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logging.warning("membuka socket")
+    # logging.warning("membuka socket")
 
     server_address = ('localhost', 45000)
-    logging.warning(f"opening socket {server_address}")
+    # logging.warning(f"opening socket {server_address}")
     sock.connect(server_address)
 
     request = 'TIME \r\n'
 
     try:
         # Send data
-        logging.warning(f"[CLIENT] request {request}")
+        # logging.warning(f"[CLIENT] request {request}")
         sock.sendall(request.encode())
         while True:
             data = sock.recv(16)
-            logging.warning(f"[WAKTU DARI SERVER] {data}")
+            # logging.warning(f"[WAKTU DARI SERVER] {data}")
     finally:
-        logging.warning("closing")
+        # logging.warning("closing")
         sock.close()
     return
 
